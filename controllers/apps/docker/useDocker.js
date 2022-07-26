fb5972acd1b9a005173b44d2367ef666bbe87dd9
bkanis <kanisbrian@gmail.com>
2022-07-26 19:27:38 -0400

    attempt at getting description

--- controllers/apps/docker/useDocker.js
+++ controllers/apps/docker/useDocker.js
@@ -27,6 +27,8 @@ const useDocker = async (apps) => {
         const names = labels['flame.name'].split(';');
         const urls = labels['flame.url'].split(';');
         let icons = '';
+        let isPublic = 0;
+        const description = labels['flame.description'].split(';');
 
         if ('flame.icon' in labels) {
           icons = labels['flame.icon'].split(';');
@@ -36,6 +38,8 @@ const useDocker = async (apps) => {
           name: names[i] || names[0],
           url: urls[i] || urls[0],
           icon: icons[i] || 'docker',
+          isPublic: isPublic[i] || '0',
+          description: description[i] || description[0],
         });
       }
     }
